58a1a9722023cf3177dba1124c9440a69eb9619e
Whyayen <whyayen@gmail.com>
2024-03-14 21:16:18 +0800

    feat: add maximum depth of binary tree

---
+++ maximum_depth_of_binary_tree.go
@@ -0,0 +1,20 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+
+	return max(left, right) + 1
+}
