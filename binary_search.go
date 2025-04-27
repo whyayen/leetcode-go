8d0541ef97635d052f7bde157790e72b43678e57
Whyayen <whyayen@gmail.com>
2025-04-27 16:05:34 +0800

    Improve code readability 704. Binary Search

--- binary_search.go
+++ binary_search.go
@@ -3,18 +3,21 @@ package main
 import "fmt"
 
 func search(nums []int, target int) int {
-	l := 0
-	r := len(nums) - 1
+	low := 0
+	high := len(nums) - 1
 
-	for l <= r {
-		mid := (l + r) / 2
+	for low <= high {
+		// 之所以 mid 不是 (low + high) / 2 是因為，這樣 low 或 high 已經到 int 極限的時候，在底層的程式相加會有溢位
+		// 而用 low + (high - low)/2 則會確保在這兩者範圍中間，且其算式是等價的
+		// 因爲可以推導為 (2 * low) / 2 + (high / 2) - (low / 2) => (2 * low + high - low) / 2 => (low + high) / 2
+		mid := low + (high-low)/2
 
-		if target == nums[mid] {
+		if nums[mid] == target {
 			return mid
-		} else if target > nums[mid] {
-			l = mid + 1
+		} else if target < nums[mid] {
+			high = mid - 1
 		} else {
-			r = mid - 1
+			low = mid + 1
 		}
 	}
 
